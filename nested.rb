9ece6c0f0cb8415f10c87a7b0ba3e327ad14824d
David Hutchison <dhutchis68516@gmail.com>
2020-05-08 18:57:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def adding_matz
      programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["Lisp", "C"]}
      #programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
      #programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp", "C"]
-     #programmer_hash
+     programmer_hash
 end
 
 def changing_alan
